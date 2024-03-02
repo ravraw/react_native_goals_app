986b72bc03dfe08dfdd5172ab9249bcbbcbbdbad
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-01 20:40:31 -0700

    Added onChange and onPress dummy methods

--- App.js
+++ App.js
@@ -2,20 +2,30 @@ import {
   Button,
   SafeAreaView,
   StyleSheet,
-  Text,
   TextInput,
+  Text,
   View,
 } from "react-native";
 
 export default function App() {
+  const onTextChange = (value) => {
+    console.log(value);
+  };
+  const addGoalHandler = () => {
+    console.log("Add goal button pressed!");
+  };
   return (
     <SafeAreaView style={styles.appContainer}>
-      <View>
-        <View style={styles.inputContainer}>
-          <TextInput placeholder="Course Goal" />
-          <Button title="ADD" />
-        </View>
-        <View></View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add a new goal!"
+          onChangeText={onTextChange}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List of goals....</Text>
       </View>
     </SafeAreaView>
   );
@@ -23,11 +33,34 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "red",
+    marginTop: 50,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "red",
   },
+  listContainer: {
+    flex: 4,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  textInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+    height: 40,
+    borderColor: "gray",
+  },
+  button: {},
 });
