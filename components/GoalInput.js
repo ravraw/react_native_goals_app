fec40f2d63e6015d268591361ad5d0c7c0c9b4e8
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-03-02 13:54:41 -0700

    Updated Styles

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     borderWidth: 1,
     padding: 10,
-    width: "80%",
+    width: "100%",
     height: 40,
     borderColor: "gray",
   },
